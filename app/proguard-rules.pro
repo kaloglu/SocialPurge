484aee9cd0907a5c7004b43130f8d84f3e761fef
Cihan Kaloğlu <cihan@kaloglu.com>
2017-11-06 01:43:26 +0300

    Implemented login with firebaseUI but doesnt work after login.

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -26,6 +26,7 @@
 
 # Add this global rule
 -keepattributes Signature
+-keepattributes *Annotation*
 
 # This rule will properly ProGuard all the model classes in
 # the package com.yourcompany.models. Modify to fit the structure
--- app/src/main/java/com/zsk/androtweet/BaseActivity.kt
+++ app/src/main/java/com/zsk/androtweet/BaseActivity.kt
@@ -8,7 +8,14 @@ import com.mikepenz.iconics.context.IconicsContextWrapper
 import com.mikepenz.iconics.context.IconicsLayoutInflater2
 
 open class BaseActivity : AppCompatActivity() {
+    private object Holder {val INSTANCE = BaseActivity()}
+
     val NO_USER: Long = -1
+    val RC_SIGN_IN: Int = 100
+    val LOGIN: Long = -99999
+    val LOGOUT: Long = -99998
+    val androTweetApp = AndroTweetApp.instance
+
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(IconicsContextWrapper.wrap(newBase))
     }
@@ -17,5 +24,9 @@ open class BaseActivity : AppCompatActivity() {
         LayoutInflaterCompat.setFactory2(layoutInflater, IconicsLayoutInflater2(getDelegate()));
 
         super.onCreate(savedInstanceState)
+    }
+
+    companion object {
+        val baseActivity: BaseActivity by lazy { BaseActivity.Holder.INSTANCE }
     }
 }
