2cd485f6b366e6034fbad44da702612c9271e49b
Cihan Kaloğlu <cihank@tuttur.com>
2017-12-22 17:44:56 +0300

    Added twitter timeline adapter

--- app/src/main/java/com/zsk/androtweet2/fragments/TimelineFragment.kt
+++ app/src/main/java/com/zsk/androtweet2/fragments/TimelineFragment.kt
@@ -1,5 +1,9 @@
 package com.zsk.androtweet2.fragments
 
+import android.support.v7.widget.RecyclerView
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
+import com.twitter.sdk.android.tweetui.TwitterListTimeline
+import com.twitter.sdk.android.tweetui.UserTimeline
 import com.zsk.androtweet2.R
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentContentTypes
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentItemTypes.LIST
@@ -11,9 +15,25 @@ import com.zsk.androtweet2.helpers.utils.Enums.FragmentTypes
 
 //TODO: update for using.
 abstract class TimelineFragment : BaseFragment() {
+    protected lateinit var timeline_rv: RecyclerView
+    protected lateinit var adapter: TweetTimelineRecyclerViewAdapter
+    protected lateinit var timeline_tweet: UserTimeline
 
     fun getInstance(@FragmentTypes fragment_type: Long, @FragmentContentTypes content_type: Long) =
             super.getInstance(fragment_type, content_type, LIST)
+
+
+    override fun initializeScreenObjects() {
+        with(view!!) {
+            timeline_rv = findViewById(R.id.timeline_rv)
+            adapter = TweetTimelineRecyclerViewAdapter(context, timeline_tweet)
+        }
+    }
+
+    override fun designScreen() {
+        timeline_rv.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
 
 }
 
--- app/src/main/java/com/zsk/androtweet2/fragments/TwitterTimelineFragment.kt
+++ app/src/main/java/com/zsk/androtweet2/fragments/TwitterTimelineFragment.kt
@@ -1,6 +1,9 @@
 package com.zsk.androtweet2.fragments
 
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
+import com.twitter.sdk.android.tweetui.UserTimeline
 import com.zsk.androtweet2.R
+import com.zsk.androtweet2.helpers.bases.BaseActivity
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentContentTypes
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentContentTypes.TWEET
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentTypes.TWITTER
@@ -15,5 +18,20 @@ class TwitterTimelineFragment : TimelineFragment() {
         get() = R.layout.twitter_timeline_layout
 
     fun getInstance(@FragmentContentTypes content_type: Long = TWEET) = super.getInstance(TWITTER, content_type)
+
+    override fun initializeScreenObjects() {
+        timeline_tweet = UserTimeline.Builder()
+                .includeRetweets(false)
+                .includeReplies(false)
+                .userId(BaseActivity.androTweetApp.accountHeader.activeProfile.identifier)
+                .build()
+
+        adapter = TweetTimelineRecyclerViewAdapter(context, timeline_tweet)
+        super.initializeScreenObjects()
+    }
+
+    override fun designScreen() {
+        super.designScreen()
+    }
 
 }
