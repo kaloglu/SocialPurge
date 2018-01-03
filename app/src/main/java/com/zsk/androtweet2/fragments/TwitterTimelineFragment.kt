e908f4d34fb3176944845098d95794617b9eb5fe
Cihan Kaloğlu <cihan@kaloglu.com>
2018-01-03 10:58:36 +0300

    Addded bottom sheet behavior visual actions.

--- app/src/main/java/com/zsk/androtweet2/fragments/TwitterTimelineFragment.kt
+++ app/src/main/java/com/zsk/androtweet2/fragments/TwitterTimelineFragment.kt
@@ -1,11 +1,14 @@
 package com.zsk.androtweet2.fragments
 
+import android.support.design.widget.BottomSheetBehavior
+import android.widget.Toast
 import com.zsk.androtweet2.R
 import com.zsk.androtweet2.adapters.AdapterFactory
 import com.zsk.androtweet2.components.twitter.UserTimeline
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentContentTypes
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentContentTypes.TWEET
 import com.zsk.androtweet2.helpers.utils.Enums.FragmentTypes.TWITTER
+import kotlinx.android.synthetic.main.actions_bottom_sheet.*
 
 /**
  * Created by kaloglu on 16.12.2017.
@@ -17,6 +20,26 @@ class TwitterTimelineFragment : TimelineFragment() {
         get() = R.layout.twitter_timeline_layout
 
     fun getInstance(@FragmentContentTypes content_type: Long = TWEET) = super.getInstance(TWITTER, content_type)
+    override fun initializeScreenObjects() {
+        super.initializeScreenObjects()
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+        select_all.setOnClickListener {
+            select_all_icon.isChecked = !select_all_icon.isChecked
+        }
+
+        select_all_icon.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(context, "select_all : " + select_all_icon.isChecked, Toast.LENGTH_SHORT).show()
+        }
+        add_queue.setOnClickListener {
+            Toast.makeText(context, "delete", Toast.LENGTH_SHORT).show()
+        }
+        open_sheet.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            else
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
 
     override fun designScreen() {
         timeline_tweet = UserTimeline()
